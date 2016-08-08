53189c442301bf8df2c550a85ff3a8fc70bfcb05
Sergey Piterman <sergeypiterman@gmail.com>
2016-08-07 23:10:00 -0700

    [dev] Add gulp scss build

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rimraf = require('rimraf');
 var sequence = require('run-sequence');
+var cleanCSS = require('gulp-clean-css');
 
 var paths = {
   styles: ['client/styles/*.scss']
@@ -11,18 +12,19 @@ gulp.task('clean', function(cb){
   rimraf('./client/build', cb);
 });
 
-gulp.task('build-css', function(){
-  return gulp.src(paths.styles)
-    .pipe(sass())
-    .pipe(gulp.dest('client/build/styles'));
+gulp.task('build-min-css', function(){ //Must run anytime SCSS is updated
+  return gulp.src(paths.styles) //SCSS files
+    .pipe(sass()) //Uses Sass Module
+    .pipe(cleanCSS({compatibility: 'ie8'})) //Minifies CSS here
+    .pipe(gulp.dest('./client/build/styles')); //Sets destination
 });
 
 gulp.task('build', function(cb){
-  sequence('clean', ['build-css'], cb);
+  sequence('clean', ['build-min-css'], cb);
 });
 
 
 
 gulp.task('default', ['build'], function(){
-  gulp.watch('client/styles/*.scss', ['build-css']);
+  gulp.watch('client/styles/*.scss', ['build-min-css']);
 });
