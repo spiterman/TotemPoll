a19510c509c45c8c0d19a3ca1872d098537045a5
Sergey Piterman <sergeypiterman@gmail.com>
2016-10-26 18:05:11 -0700

    [feat] Change server entry point

---
+++ index.js
@@ -0,0 +1,9 @@
+var config = require('./server/config/config');
+var app = require('./server/server.js');
+var logger = require('./server/util/logger');
+
+app.listen(config.port);
+console.log('Server listening on http://localhost:' + config.port)
+// logger.log('Server listening on http://localhost:' + config.port);
+
+
