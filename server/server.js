73f159d909167c4f8256c79871acd3fbe2467793
Sergey Piterman <sergeypiterman@gmail.com>
2016-10-26 18:04:40 -0700

    [refactor] Restructure backend application

--- server/server.js
+++ server/server.js
@@ -1,15 +1,34 @@
-'use strict'
-
+// Set up Express app
 var express = require('express');
-var http = require('http');
 var app = express();
-var server = http.createServer(app);
-var port = process.env.PORT || 3000;
-var middleware = require('./config/middleware');
 
-middleware(app, express);
+// Load Modules
+var api = require('./api');
+var config = require('./config/config');
+var logger = require('./util/logger');
+var auth = require('./auth/routes');
 
-server.listen(port, function(){
-  console.log('Server listening on ', port);
-})
+// Database Setup
+var mongoose = require('mongoose');
+mongoose.connect(config.db.url);
+
+// Global Middleware
+require('./middleware/appMiddleware')(app);
+
+// API Setup
+app.use('/api', api);
+app.use('/auth', auth);
+
+// Global Error Handling
+app.use(function(err, req, res, next) {
+  if(err.name === 'UnauthorizedError') {
+    res.status(401).send('Invalid token');
+    return;
+  }
+  logger.error(err.stack);
+  res.status(500).send('Oops');
+});
+
+// Export App (For Testing)
+module.exports = app;
 
