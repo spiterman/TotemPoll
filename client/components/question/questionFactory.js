629feb5ca2d37067ef6081682a8bcaf3102ecc14
Sergey Piterman <sergeypiterman@gmail.com>
2016-11-15 14:53:36 -0800

    [feat] Add question factory outline

--- client/components/question/questionFactory.js
+++ client/components/question/questionFactory.js
@@ -3,21 +3,51 @@
     .factory('QuestionFactory', ['$http', function($http){
 
       var model = {};
-      model.testPost = function(data){
-        console.log(data);
+
+      model.getAllQuestions = function(){
         return $http({
-          method: 'POST',
-          url: '/api/users',
-          data: data
-        })
-        .then(function(responseObj){
-          return responseObj;
+          method: 'GET',
+          url: 'api/questions'
         });
       };
 
-      model.testGet = function() {
-        return;
-      }
+      model.getNextQuestion = function(){
+        return $http({
+          method: 'GET',
+          url: '/api/questions/next'
+        });
+      };
+
+      model.agree = function(){
+        return $http({
+          method: 'POST',
+          url: '/api/userQJoin',
+          data: 'agree'
+        });
+      };
+
+      model.disagree = function(){
+        return $http({
+          method: 'POST',
+          url: '/api/userQJoin',
+          data: 'disagree'
+        });
+      };
+
+      model.pass = function(){
+        return $http({
+          method: 'POST',
+          url: '/api/userQJoin',
+          data: 'pass'
+        });
+      };
+
+      model.getComments = function(){
+        return $http({
+          method: 'GET',
+          url: '/api/userQJoin'
+        });
+      };
 
       return model;
 
