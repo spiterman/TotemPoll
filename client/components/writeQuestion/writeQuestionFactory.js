f1100daa3e34587da95c128252ba79567a56fccd
Sergey Piterman <sergeypiterman@gmail.com>
2016-11-15 21:02:46 -0800

    [feat] Add writeQuestionFactory outline

---
+++ client/components/writeQuestion/writeQuestionFactory.js
@@ -0,0 +1,10 @@
+;(function(){
+angular.module('app.writeQuestion')
+  .factory('WriteQuestionFactory', ['$http', function($http){
+    var model = {};
+
+    model.submit = function(){};
+
+    return model;
+  }]);
+})();
