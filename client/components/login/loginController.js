f37a4a4bb81af61544da281c9aaeb4f2031b8e1f
Sergey Piterman <sergeypiterman@gmail.com>
2016-11-30 20:55:26 -0800

    [feat] Streamline login functionality

--- client/components/login/loginController.js
+++ client/components/login/loginController.js
@@ -1,30 +1,27 @@
 (function(){
 
-  angular.module('app.login', ['LocalStorageModule'])
-    .config(function(localStorageServiceProvider){
-      localStorageServiceProvider
-        .setPrefix('testing');
-    })
+  angular.module('app.login', [])
     .controller('LoginController', ['LoginFactory', 'localStorageService', '$state', function(LoginFactory, localStorageService, $state) {
 
       var vm = this;
-
-      vm.userData = {}
+      vm.userData = {};
+      vm.userData.name = '';
       vm.userData.username = '';
       vm.userData.password = '';
 
       vm.signUp = function(){
-
+        console.log('Sign Up Called')
         LoginFactory.signUp(vm.userData)
           .then(function(response){
             console.log('got the response');
-            console.log(response);
+            console.log(response.data.user.username);
             localStorageService.set('token', response.data.token);
-          })
+            $state.go('/profile/' + response.data.user.username);
+          });
 
         // Reset names
-        // vm.username = '';
-        // vm.password = '';
+        // vm.userData.username = '';
+        // vm..userData.password = '';
       };
 
       vm.signIn = function(){
@@ -33,11 +30,35 @@
             console.log('signed in');
             console.log(response);
             localStorageService.set('token', response.data.token);
-          })
-          .then(function(){
-            $state.go('question');
+            $state.go('profile', {username: response.data.user.username});
           });
       };
+
+      // Handle Sign In vs Sign Up
+      var signIn = {
+        alternateAction: 'Sign Up',
+        alternateMessage: 'New to us?',
+        action: 'Sign In',
+        fn: vm.signIn
+      };
+
+      var signUp = {
+        alternateAction: 'Sign In',
+        alternateMessage: 'Already have an account? ',
+        action: 'Sign Up',
+        fn: vm.signUp
+      };
+
+      vm.loginState = signIn;
+
+      vm.toggleLoginState = function(){
+        if(vm.loginState === signIn){
+          vm.loginState = signUp;
+        } else {
+          vm.loginState = signIn;
+        }
+      };
+
 
 
     }]);
